06c042734300b9bdceede4a956666f22e56a6aa5
codemachine69 <66106096+codemachine69@users.noreply.github.com>
2020-07-02 17:59:42 +0530

    Add files via upload

---
+++ firstCV.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+while True:
+    success, img = cap.read()
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(imgGray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
+    cv2.imshow('Video', img)
+    if cv2.waitKey(1) & 0xff==ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
+cv2.imshow('Virat', img)
+cv2.waitKey(0)
+
+
